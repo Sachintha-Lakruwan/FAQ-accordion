35f4b291924f1ad8a67fb4885bf5b8f5967201c3
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2024-06-11 22:59:37 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        p1: "#F9EEFF",
+        p2: "#A034C9",
+        p3: "#331539",
+        pdull: "#A597A8",
+      },
+    },
+  },
+  plugins: [],
+};
